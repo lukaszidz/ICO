2f4c289623bddf1b1497a667b547bffe0b650229
Łukasz Idziak <lidziak@ah4r.com>
2023-06-05 15:22:30 +0200

    Add contract and NextJs app

---
+++ hardhat-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x457941D2e93b7A515649cdaDeb93b3524Eb16739";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
